50fb2d967ddc724dec3257c587e2ea4c5264422d
Caio Rodrigues <rfcaio90@gmail.com>
2020-06-05 10:38:25 -0300

    add cpf format validation

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const CPF_REGEX = /^\d{11}$/
+
 class Cpf {
   constructor (value) {
     if (typeof value !== 'string') {
       throw TypeError()
+    }
+
+    if (!CPF_REGEX.test(value)) {
+      throw Error()
     }
 
     this._value = value
--- test.js
+++ test.js
@@ -8,6 +8,10 @@ describe('Cpf', () => {
       expect(() => new Cpf(20422290084)).toThrowError(TypeError)
     })
 
+    test('throw an Error if cpf is not an eleven character numeric string', () => {
+      expect(() => new Cpf('204.222.900-84')).toThrowError(Error)
+    })
+
     test('create a new cpf', () => {
       expect(new Cpf('20422290084')).toEqual({ _value: '20422290084' })
     })
