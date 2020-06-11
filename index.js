679d8c272459ef37ecf457c7fc9e9d2c5b40e6a7
Caio Rodrigues <rfcaio90@gmail.com>
2020-06-11 18:12:42 -0300

    improve cpf validation

--- index.js
+++ index.js
@@ -21,12 +21,31 @@ class Cpf {
     throw new Error()
   }
 
+  static _getVerifyDigit (cpf) {
+    const digitSum =
+      Array
+        .from(cpf)
+        .reduce(
+          (sum, digit, index) => (
+            sum + parseInt(digit, 10) * (cpf.length + 1 - index)
+          ), 0
+        )
+    const factor = digitSum % 11
+    return `${factor > 2 ? 11 - factor : 0}`
+  }
+
   static isValid (cpf) {
-    const EQUAL_DIGITS_CPFS = Array.from({ length: 10 }, (_, i) => i.toString().repeat(11))
+    const EQUAL_DIGITS_CPFS =
+      Array.from({ length: 10 }, (_, i) => i.toString().repeat(11))
+
     if (EQUAL_DIGITS_CPFS.includes(cpf)) {
       return false
     }
-    return true
+    const firstNineDigits = cpf.slice(0, 9)
+    const firstCheckDigit = Cpf._getVerifyDigit(firstNineDigits)
+    const secondCheckDigit =
+      Cpf._getVerifyDigit(firstNineDigits + firstCheckDigit)
+    return cpf === firstNineDigits + firstCheckDigit + secondCheckDigit
   }
 }
 
--- test.js
+++ test.js
@@ -35,5 +35,13 @@ describe('Cpf', () => {
     test('return false if cpf has eleven equal digits', () => {
       expect(Cpf.isValid('00000000000')).toBeFalsy()
     })
+
+    test('return false if cpf is not validated internally', () => {
+      expect(Cpf.isValid('20422290080')).toBeFalsy()
+    })
+
+    test('return true if cpf is validated internally', () => {
+      expect(Cpf.isValid('20422290084')).toBeTruthy()
+    })
   })
 })
