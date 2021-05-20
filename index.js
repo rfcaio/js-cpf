41f4ec7aeccf40f99a83d633ee00ad4ccc3253b1
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-20 00:29:19 -0300

    change project index file

---
+++ index.js
@@ -0,0 +1,5 @@
+const Cpf = require('./Cpf')
+const CpfGenerator = require('./CpfGenerator')
+const InvalidCpfError = require('./InvalidCpfError')
+
+module.exports = { Cpf, CpfGenerator, InvalidCpfError }
