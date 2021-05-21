396189b2fc7c742f358e448f0dd35a576e2a975d
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-21 00:59:56 -0300

    change CPF generation function

--- CpfGenerator.js
+++ CpfGenerator.js
@@ -1,25 +1,15 @@
-const randint = require('@rfcaio/randint')
-
 const Cpf = require('./Cpf')
 
-const { getVerifyDigitFrom } = require('./utils')
+const { generateNineDigits, getVerifyDigitFrom } = require('./utils')
 
 class CpfGenerator {
   generate () {
-    const firstNineDigits = this._getFirstNineDigits()
+    const firstNineDigits = generateNineDigits()
     const firstVerifyDigit = getVerifyDigitFrom(firstNineDigits)
     const secondVerifyDigit = getVerifyDigitFrom(
       firstNineDigits + firstVerifyDigit
     )
     return new Cpf(firstNineDigits + firstVerifyDigit + secondVerifyDigit)
-  }
-
-  _getFirstNineDigits () {
-    let result = ''
-    for (let i = 0; i < 9; i += 1) {
-      result += String(randint())
-    }
-    return result
   }
 }
 
--- CpfGenerator.test.js
+++ CpfGenerator.test.js
@@ -1,27 +1,24 @@
 /* eslint-env jest */
 
-const randint = require('@rfcaio/randint')
-
 const Cpf = require('./Cpf')
 const CpfGenerator = require('./CpfGenerator')
 
-jest.mock('@rfcaio/randint')
+require('./utils')
+
+/* TODO: improve this mock implementation */
+jest.mock('./utils', () => {
+  return {
+    generateNineDigits: jest.fn().mockReturnValue('095167763'),
+    getVerifyDigitFrom: jest
+      .fn()
+      .mockReturnValueOnce('2')
+      .mockReturnValueOnce('9')
+  }
+})
 
 describe('CpfGenerator', () => {
   describe('generate', () => {
     test('returns a new instance of Cpf class with randomic value', () => {
-      randint
-        .mockName('randint')
-        .mockReturnValueOnce(0)
-        .mockReturnValueOnce(9)
-        .mockReturnValueOnce(5)
-        .mockReturnValueOnce(1)
-        .mockReturnValueOnce(6)
-        .mockReturnValueOnce(7)
-        .mockReturnValueOnce(7)
-        .mockReturnValueOnce(6)
-        .mockReturnValueOnce(3)
-
       const cpfGenerator = new CpfGenerator()
       const cpf = cpfGenerator.generate()
 
