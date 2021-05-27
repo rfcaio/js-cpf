5e8eacf162092e46adc91ac68d22a22a8ed2845a
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-27 15:28:39 -0300

    change CPF generator class test

--- CpfGenerator.test.js
+++ CpfGenerator.test.js
@@ -3,19 +3,6 @@
 const Cpf = require('./Cpf')
 const CpfGenerator = require('./CpfGenerator')
 
-require('./utils')
-
-/* TODO: improve this mock implementation */
-jest.mock('./utils', () => {
-  return {
-    generateNineDigits: jest.fn().mockReturnValue('095167763'),
-    getVerifyDigitFrom: jest
-      .fn()
-      .mockReturnValueOnce('2')
-      .mockReturnValueOnce('9')
-  }
-})
-
 describe('CpfGenerator', () => {
   describe('generate', () => {
     test('returns a new instance of Cpf class with randomic value', () => {
@@ -23,7 +10,6 @@ describe('CpfGenerator', () => {
       const cpf = cpfGenerator.generate()
 
       expect(cpf instanceof Cpf).toEqual(true)
-      expect(cpf.value).toEqual('09516776329')
     })
   })
 })
